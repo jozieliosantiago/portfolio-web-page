8741af0643baa823cf34f407e6240f5e60983e4c
Joziélio Santiago <jozieliosantiago@gmail.com>
2020-02-25 23:48:33 -0300

    new elements added

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import Repositories from '../../components/Repositories';
+import Presentation from '../../components/Presentation';
+import { Container } from '../../styles/global';
 
-const Home = () => <Repositories />;
+const Home = () => {
+  return (
+    <Container>
+      <Presentation />
+      <Repositories />
+    </Container>
+  )
+}
 
 export default Home;
