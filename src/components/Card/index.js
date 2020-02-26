c039e96087e4b571957452e69208377b2876a1e8
Joziélio Santiago <jozieliosantiago@gmail.com>
2020-02-25 23:42:08 -0300

    new props added

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,7 +5,7 @@ import Icon from '../Icon';
 const Card = ({ language, link, title, description }) => {
   return (
     <Container>
-      <Icon language={language} />
+      <Icon language={language} title={title} />
       <h4><a href={link} target="_blank" rel="noopener noreferrer">{title}</a></h4>
       <p>{description}</p>
     </Container>
